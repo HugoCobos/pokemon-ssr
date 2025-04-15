ac90b9c5ce0eed098e8e2aacadbc042608c2c66e
Hugo Cobos <hugocobos97@gmail.com>
2025-04-15 13:55:26 -0600

    first commit

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import bootstrap from './main.server';
 
+import { render } from '@netlify/angular-runtime/common-engine.mjs';
+
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 const indexHtml = join(serverDistFolder, 'index.server.html');
@@ -24,6 +26,20 @@ const commonEngine = new CommonEngine();
  * ```
  */
 
+export async function netlifyCommonEngineHandler(
+  request: Request,
+  context: any
+): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine);
+}
+
 /**
  * Serve static files from /browser
  */
@@ -31,8 +47,8 @@ app.get(
   '**',
   express.static(browserDistFolder, {
     maxAge: '1y',
-    index: 'index.html'
-  }),
+    index: 'index.html',
+  })
 );
 
 /**
